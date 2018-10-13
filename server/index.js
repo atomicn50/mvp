dd8cd400edbf8993ea1e8804a537bc1d679e3fcf
Tinh <tinhkle@Doxie.local>
2018-10-13 15:07:59 -0700

    configures project files

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+app.listen(PORT, () => {
+  console.log(`Listening at localhost on port ${PORT}`);
+});
